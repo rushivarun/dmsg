78f60bee8fa6db100ef6d41c9616aa67acf6916f
rushivarun <rushi2000varun@gmail.com>
2020-08-26 14:02:10 +0530

    server ready

---
+++ go.mod
@@ -0,0 +1,14 @@
+module dmsg
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/kisielk/gotool v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202
+	golang.org/x/sys v0.0.0-20200824131525-c12d262b63d8 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/grpc v1.31.1
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"dmsg/proto"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	"google.golang.org/grpc"
+	glog "google.golang.org/grpc/grpclog"
+)
+
+var grpcLog glog.LoggerV2
+
+func init() {
+	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
+
+}
+
+// Connection struct is the type of data for a connection
+type Connection struct {
+	stream proto.Broadcast_CreateStreamServer
+	id     string
+	active bool
+	topic  string
+	error  chan error
+}
+
+// Server is a collection of all the succesful connections to create a stream
+type Server struct {
+	Connection []*Connection
+}
+
+// CreateStream ensures the successful connection to start stream
+func (s *Server) CreateStream(pconn *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
+	conn := &Connection{
+		stream: stream,
+		id:     pconn.User.Id,
+		active: true,
+		topic:  pconn.Topic.Name,
+		error:  make(chan error),
+	}
+
+	s.Connection = append(s.Connection, conn)
+
+	return <-conn.error
+}
+
+// BroadcastMessage is responsible for sending out messages
+func (s *Server) BroadcastMessage(ctx context.Context, msg *proto.Message) (*proto.Close, error) {
+	wait := sync.WaitGroup{}
+	done := make(chan int)
+
+	for _, conn := range s.Connection {
+		wait.Add(1)
+
+		go func(msg *proto.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err := conn.stream.Send(msg)
+				if err != nil {
+					grpcLog.Errorf("Error with Stream: %v , on topic: %v - Error: %v", conn.stream, conn.topic, err)
+					conn.active = false
+					conn.error <- err
+				}
+				grpcLog.Info("Sending message to topic: ", conn.topic)
+
+			}
+		}(msg, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	return &proto.Close{}, nil
+}
+
+func main() {
+	var connections []*Connection
+
+	server := &Server{connections}
+
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal("Error while listening on port 8000: ", err)
+	}
+
+	grpcLog.Info("Starting server on port 8000")
+
+	proto.RegisterBroadcastServer(grpcServer, server)
+	grpcServer.Serve(listener)
+
+}
